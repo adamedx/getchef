7c3c181ae49d75c2c0647ae67a71f53e317a8dc5
adamedx <adamed@getchef.com>
2015-05-15 22:55:32 -0700

    Support redirects

--- getchefdk-stable.sh
+++ getchefdk-stable.sh
@@ -1,2 +1,4 @@
-curl https://packagecloud.io/install/repositories/chef/stable/script.deb | sudo bash
+apt-get update
+apt-get install curl
+curl -L https://packagecloud.io/install/repositories/chef/stable/script.deb | sudo bash
 apt-get install chefdk
